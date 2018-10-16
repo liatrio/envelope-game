1d39525a86aad911078b81b7922afe444c6f71dc
Robert Kelly <RobertKelly@users.noreply.github.com>
2018-10-15 21:43:21 -0500

    Updating some configuration

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 console.log('Server-side code running');
-
 var express = require(‘express’);
 var port = process.env.PORT || 3000;
 var app = express();
-// serve files from the public directory
-app.use(express.static('public'));
-
+app.get('/', (req, res) => {
+    res.sendFile(public + '/index.html');
+});
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
-});
-
-// serve the homepage
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
 });
