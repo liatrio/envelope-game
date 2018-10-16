7037a80afe3c22ac27764ba7c3ae8b46274869a9
Robert Kelly <RobertKelly@users.noreply.github.com>
2018-10-15 21:54:23 -0500

    Checking for app typos

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 console.log('Server-side code running');
-var express = require(‘express’);
+var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 app.get('/', (req, res) => {
-    res.sendFile(public + '/index.html');
+    res.sendFile('/public/index.html');
 });
 app.listen(port, function () {
  console.log('App running on port: '+port);
