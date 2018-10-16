67700d7283d500d553bdbc364a7354a99ee8ed78
Robert Kelly <RobertKelly@users.noreply.github.com>
2018-10-15 22:17:09 -0500

    Updating to default requests

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ console.log('Server-side code running');
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.sendFile('/public/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 app.listen(port, function () {
  console.log('App running on port: '+port);
