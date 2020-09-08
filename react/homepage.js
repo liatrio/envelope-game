f3c94e9a108a6b17e4b7342921b04a10459e38c5
Brad <bradf@liatrio.com>
2020-09-08 14:31:57 -0700

    About Section Added

--- react/homepage.js
+++ react/homepage.js
@@ -9,6 +9,12 @@ class homepage extends Component {
             <div>
             <h1>The Envelope Game</h1>
             <h5>About The Game</h5>
+            - The Envelope Game is a great exercise to show the differences in the different delivery methods.
+            <br/>- There will be two evenly numbered teams; one team for Batch Delivery and one team for Flow Delivery
+            <br/>- Both teams will be given the task of using teamwork to move along the envelopes through the team into delivery of the envelopes
+            <br/>- Each person on the batch team will be designated to work with ten envelopes at a time before handing off to the next partner in their team
+            <br/>- The Flow Team will have the luxury of passing on the envelopes to their team-mates right away
+            <br/>
             <button onClick={this.createGame}>
             Create a Game    
             </button>
