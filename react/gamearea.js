e749ad754866afeba59de19fa8aeae196d650388
Ryan McGuire <ryanadmcguire@gmail.com>
2020-09-10 16:05:52 -0700

    start adding ways to control the game

--- react/gamearea.js
+++ react/gamearea.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import Controls from './controls'
 
 class Gamearea extends Component {
+    constructor() {
+        super();
+    }
     render() {
+        console.log(this.props.location.facilitatorID)
         return (
             <div>
                 Game Area
+                <Controls />
             </div>
         )
     }
