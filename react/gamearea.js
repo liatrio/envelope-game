735999e6f99e21796446773ef7ab026f2a3367ba
Brad <bradf@liatrio.com>
2020-09-14 12:38:08 -0700

    add async componentDidMount

--- react/gamearea.js
+++ react/gamearea.js
@@ -1,37 +1,58 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faChair} from '@fortawesome/free-solid-svg-icons'
+import {useParams} from 'react-router-dom'
 
 class Gamearea extends Component {
     constructor(props)
     {
         super(props);
         this.state = {
-            color: Array(6).fill('black')
+            gameID: null,
+            color: Array(6).fill('black'),
+            isStarted: false,
+            team1Seats: [],
+            team2Seats: []
         }
     }
-    chooseSeat(c, index)
+    chooseSeat(index)
     {
         let colorChoice = this.state.color;
         colorChoice[index] = 'blue';
         console.log(index);
         this.setState({color: colorChoice});
     }
+
+    async componentDidMount() {
+        let gameID = useParams();
+        console.log(gameID);
+        const response = await fetch('/api/join/{gameID}')
+        const json = await response.json();
+        this.setState({isStarted: json.isStarted, })
+    }
+
     render() {
-        //const = [FontAwesomeIcon icon={faChair} size = '7x' color={this.state.color[0]]
+        
         var chairs = this.state.color;
-        console.log(chairs);
+        var team1Chairs = [];
+        var team2Chairs = [];
+        /* chairs.forEach(c, index => {
+            if (c.isTeamOne) {
+              team1Chairs.append(<li><button onClick={() => this.chooseSeat(index)}>
+              <FontAwesomeIcon icon={faChair} size = '7x' color={c} /><br/>
+          </button></li>);
+            }
+            else {
+                team2Chairs.append(<li><button onClick={() => this.chooseSeat(index)}>
+              <FontAwesomeIcon icon={faChair} size = '7x' color={c} /><br/>
+          </button></li>);
+            }  
+        }); */
+
         return (
             <div>
                 Game Area<br/>
-                <ul>
-                    {chairs.map((c, index) =>
-                    <button onClick={() => this.chooseSeat(chairs, index)}>
-                        <FontAwesomeIcon key={c.toString} icon={faChair} size = '7x' color={c} /><br/>
-                    </button>)}
-                </ul>
-                
-                
+                <ul>{team1Chairs}</ul>
             </div>
         )
     }
