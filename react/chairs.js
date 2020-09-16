36360c828c24b6e73f9c4c14bb2be6dd1dbd62bc
Brad <bradf@liatrio.com>
2020-09-16 15:04:58 -0700

    Add bootstrap to set chair into two columns

--- react/chairs.js
+++ react/chairs.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faChair} from '@fortawesome/free-solid-svg-icons'
-import {useParams} from 'react-router-dom'
+import {faSquareFull} from '@fortawesome/free-solid-svg-icons'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import './index.css'
 class Chairs extends Component {
     constructor(props)
     {
@@ -19,27 +23,32 @@ class Chairs extends Component {
 render() {
     var team1Chairs = []
     var team2Chairs = []
+    //var workArea = []
     var chairs = this.props.seats;
     const gameID = this.props.gameID;
     chairs.forEach((c, index) => {
-    //console.log(c.is_taken);
     if (c.is_team_1) {
         team1Chairs.push(<li><button className={c.is_taken ? "chairFilled" : "chairNotFilled"} disabled={c.is_taken ? true : false} onClick={() => this.chooseSeat(index, c.seat_id, gameID)}>
-        <FontAwesomeIcon icon={faChair} size = '7x' color={c.is_taken ? 'blue' : 'black'} /><br/>
-        </button></li>);
+        <FontAwesomeIcon icon={faChair} size = '7x' color={c.is_taken ? 'blue' : 'black'} />
+        </button> <FontAwesomeIcon icon={faSquareFull} size ='7x' color='brown'/></li>);
+        
     }
     else {
-    team2Chairs.push(<li><button className={c.is_taken ? "chairFilled" : "chairNotFilled"} disabled={c.is_taken ? true : false} onClick={() => this.chooseSeat(index, c.seat_id, gameID)}>
-        <FontAwesomeIcon icon={faChair} size = '7x' color={c.is_taken ? 'blue' : 'black'} /><br/>
+    team2Chairs.push(<li><FontAwesomeIcon icon={faSquareFull} size ='7x' color='brown'/>
+    <button className={c.is_taken ? "chairFilled" : "chairNotFilled"} disabled={c.is_taken ? true : false} onClick={() => this.chooseSeat(index, c.seat_id, gameID)}>
+        <FontAwesomeIcon icon={faChair} size = '7x' color={c.is_taken ? 'blue' : 'black'} />
     </button></li>);
-            }  
+    }
+    //workArea.push()  
         });
     return (
         <div>
-        <ul>{team1Chairs}
-        </ul>
-        <ul>{team2Chairs}
-        </ul>
+            <Container>
+                <Row>
+                <Col><ul className = "chairColumn">{team1Chairs}</ul></Col>
+                <Col><ul className = "chairColumn">{team2Chairs}</ul></Col>
+                </Row>
+            </Container>
         </div>
     )
     }
