4f379786f8af81120f5d734f2ec1cbd602372485
Brad <bradf@liatrio.com>
2020-09-17 10:06:52 -0700

    Add active clause on buttons

--- react/chairs.js
+++ react/chairs.js
@@ -17,10 +17,14 @@ class Chairs extends Component {
     }
     async chooseSeat(index, seat_id, gameID)
     {
-        this.setState({seatID: seat_id});
-        const response = await fetch(`/api/choose-seat/${gameID}/${seat_id}`)
-        const json = await response.json();
-        console.log(json);
+        if (!this.state.seatID)
+        {
+            const response = await fetch(`/api/choose-seat/${gameID}/${seat_id}`)
+            const json = await response.json();
+            console.log(json);
+            console.log(this.state.seatID);
+            this.setState({seatID: seat_id});
+        }
         // api/chooseSeat/gameID/seatID
     }
 
@@ -32,16 +36,15 @@ render() {
     const gameID = this.props.gameID;
     chairs.forEach((c, index) => {
     if (c.is_team_1) {
-        team1Chairs.push(<li><ToggleButton className={c.is_taken ? "chairFilled" : "chairNotFilled"} disabled={this.state.seatID !== null ? 'true' : 'false'} onClick={() => this.chooseSeat(index, c.seat_id, gameID)}>
-        <FontAwesomeIcon icon={faChair} size = '7x' color={c.is_taken ? 'blue' : 'black'} />
+        team1Chairs.push(<li><ToggleButton className={c.is_taken ? "chairFilled" : "chairNotFilled"} type="radio" variant="secondary" active={c.is_taken ? 'true' : 'false'} onClick={() => this.chooseSeat(index, c.seat_id, gameID)}>
+        <FontAwesomeIcon icon={faChair} size = '7x' color={c.seat_id === this.state.seatID ? 'blue' : c.is_taken ? 'red' : 'black'} />
         </ToggleButton> <FontAwesomeIcon icon={faSquareFull} size ='7x' color='brown'/></li>);
-        
     }
     else {
-    team2Chairs.push(<li><FontAwesomeIcon icon={faSquareFull} size ='7x' color='brown'/>
-    <ToggleButton className={c.is_taken ? "chairFilled" : "chairNotFilled"} disabled={this.state.seatID !== null ? 'true' : 'false'} onClick={() => this.chooseSeat(index, c.seat_id, gameID)}>
-        <FontAwesomeIcon icon={faChair} size = '7x' color={c.is_taken ? 'blue' : 'black'} />
-    </ToggleButton></li>);
+        team2Chairs.push(<li><FontAwesomeIcon icon={faSquareFull} size ='7x' color='brown'/>
+        <ToggleButton className={c.is_taken ? "chairFilled" : "chairNotFilled"} type="radio" variant="secondary" active={c.is_taken ? 'true' : 'false'} onClick={() => this.chooseSeat(index, c.seat_id, gameID)}>
+            <FontAwesomeIcon icon={faChair} size = '7x' color={c.seat_id === this.state.seatID ? 'blue' : c.is_taken ? 'red' : 'black'} />
+        </ToggleButton></li>);
     }
     //workArea.push()  
         });
