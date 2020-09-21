5032d94377758f33e09b6b92f66dcce0c2407162
Paul Henson <paulh@liatrio.com>
2020-09-21 14:07:04 -0700

    Fixed Merge Conflict

--- react/gamearea.js
+++ react/gamearea.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import Gameprogress from './gameprogress';
 import ChairsCollection from './chair_collection';
 import Controls from './controls'
+
 import './index.css'
+
 import EnvelopeStack from './envelope_stack'
 
 class Gamearea extends Component {
@@ -81,7 +83,7 @@ class Gamearea extends Component {
           Game Area
           <EnvelopeStack></EnvelopeStack>
           <Gameprogress t1Name={ this.state.teamName_1 } t1Begin={4} t1End={9} t2Name={ this.state.teamName_2 } t2Begin={1} t2End={2} />
-          <ChairsCollection seats={this.state.seats} gameID={this.props.match.params.gameID} setSeatId={(id) => this.setSeatId(id)} playerSeatId={this.state.seatId} display_name= 'me'/>
+          <ChairsCollection seats={this.state.seats} gameID={this.props.match.params.gameID} setSeatId={(id) => this.setSeatId(id)} playerSeatId={this.state.seatId}/>
           <Controls facilitatorGets={this.props.location.state.facilitatorID} team_id_1={this.state.team_id_1} team_id_2={this.state.team_id_2} />
         </div>
       );
