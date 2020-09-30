3ce1912d5720f7ce9a25c821cb32d86406de4a3b
Ryan McGuire <ryanadmcguire@gmail.com>
2020-09-30 14:03:14 -0700

    format server.js

--- server.js
+++ server.js
@@ -11,28 +11,28 @@ const webpackConfig = require('./webpack.config');
 const compiler = webpack(webpackConfig);
 
 if (webpackConfig.mode === 'production') {
-    app.use(express.static(path.join(__dirname, 'dist')));
-    const routes = require('./routes');
-    app.use(routes);
+  app.use(express.static(path.join(__dirname, 'dist')));
+  const routes = require('./routes');
+  app.use(routes);
 } else {
-    app.use(middleware(compiler));
-    app.use(hotMiddleware(compiler));
+  app.use(middleware(compiler));
+  app.use(hotMiddleware(compiler));
 
-    require('node-hot')
-        .configure({
-            exclude: [
-                /[\/\\]node_modules[\/\\]/,
-            ],
-        });
-
-    app.use((req, res, next) => {
-        require('./routes').handle(req, res, next);
+  require('node-hot')
+    .configure({
+      exclude: [
+        /[\/\\]node_modules[\/\\]/,
+      ],
     });
+
+  app.use((req, res, next) => {
+    require('./routes').handle(req, res, next);
+  });
 }
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
+  console.log(`Listening on port ${port}...`);
 });
 
