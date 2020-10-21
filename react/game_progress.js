ddff99a841df592ae8f838561170ab9993f4f953
Brad <bradf@liatrio.com>
2020-10-21 14:28:44 -0700

    Add no spin on play button

--- react/game_progress.js
+++ react/game_progress.js
@@ -43,12 +43,11 @@ class GameProgress extends Component {
     const facilID = this.props.facilitatorId;
     return (
       <div>
-        <div>
         {facilID &&
           <Row className= "justify-content-md-center">
             <FontAwesomeIcon className="playIcon" 
             icon={ic} 
-            spin onClick={this.props.seatsFull ? this.togglePlay : this.seatsNotFull} 
+            onClick={this.props.seatsFull ? this.togglePlay : this.seatsNotFull} 
             disabled={this.state.disabled} />
           </Row>
         }
@@ -58,7 +57,6 @@ class GameProgress extends Component {
         {facilID && this.state.seatsFullError &&
           <Row className= "justify-content-md-center">Error: Seats are not Full yet</Row>
         }
-        </div>
         <div style={{width: "35%", top: "15%", left: "50%", marginLeft: "-17.5%", position: "absolute"}}>
           <Card.Img as={Corkboard} alt="Scoreboard"/>    
           <Card.ImgOverlay bsPrefix='card-img-overlay CardImgOverlay'>
