17c0ff181d4b8635dea36a461e8132e7e8c526d2
Chris Warren <chrisw@liatrio.com>
2022-03-31 16:32:20 -0500

    Update db.js

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ function getDb() {
       host: process.env.DB_HOST,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: 'envelope_game'
+      database: 'envelope-game'
     });
   }
   
