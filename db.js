e8b10fc8c54a4ddf8e95c880348dcf3ddb0fc40b
wai-calvin <calvinw@liatrio.com>
2022-03-31 09:28:07 -0700

    chore: add in db values for db connection for local testing

--- db.js
+++ db.js
@@ -4,13 +4,58 @@ var db;
 function getDb() {
   if (!db) {
     db = mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      host: 'envelope-game.mysql.database.azure.com',
+      user: 'envelope-game@envelope-game',
+      password: 'J2g{G)YG1qv3nQ=k',
       database: 'envelope-game'
     });
   }
+
+  db.connect(
+    function(err) {
+      if (err) {
+        console.log("Cannot connect, error: ");
+        throw err;
+      }
+      else {
+        console.log("Connection established");
+        initData();
+      }
+    }
+  );
+
   return db;
 }
 
 module.exports = getDb();
+
+// const mysql = require('mysql');
+// const fs = require('fs');
+
+// var config = {
+//   host: 'envelope-game.mysql.database.azure.com',
+//   user: 'envelope-game@envelope-game',
+//   password: 'J2g{G)YG1qv3nQ=k',
+//   database: 'envelope-game',
+//   port: 3306
+//   // ssl:
+// };
+
+// const conn = new mysql.createConnection(config);
+
+// conn.connect(
+//   function(err) {
+//     if (err) {
+//       console.log("Cannot connect, error: ");
+//       throw err;
+//     }
+//     else {
+//       console.log("Connection established");
+//       initData();
+//     }
+//   }
+// );
+
+// function initData() {
+
+// }
