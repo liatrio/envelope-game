781495b3466ea421573548e23407d82f80362c4b
Chris Warren <chrisw@liatrio.com>
2022-03-31 15:47:38 -0500

    clean up db.js

--- db.js
+++ db.js
@@ -4,58 +4,14 @@ var db;
 function getDb() {
   if (!db) {
     db = mysql.createConnection({
-      host: 'envelope-game.mysql.database.azure.com',
-      user: 'envelope-game@envelope-game',
-      password: 'J2g{G)YG1qv3nQ=k',
-      database: 'envelope-game'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: 'envelope_game'
     });
   }
-
-  db.connect(
-    function(err) {
-      if (err) {
-        console.log("Cannot connect, error: ");
-        throw err;
-      }
-      else {
-        console.log("Connection established");
-        initData();
-      }
-    }
-  );
 
   return db;
 }
 
 module.exports = getDb();
-
-// const mysql = require('mysql');
-// const fs = require('fs');
-
-// var config = {
-//   host: 'envelope-game.mysql.database.azure.com',
-//   user: 'envelope-game@envelope-game',
-//   password: 'J2g{G)YG1qv3nQ=k',
-//   database: 'envelope-game',
-//   port: 3306
-//   // ssl:
-// };
-
-// const conn = new mysql.createConnection(config);
-
-// conn.connect(
-//   function(err) {
-//     if (err) {
-//       console.log("Cannot connect, error: ");
-//       throw err;
-//     }
-//     else {
-//       console.log("Connection established");
-//       initData();
-//     }
-//   }
-// );
-
-// function initData() {
-
-// }
