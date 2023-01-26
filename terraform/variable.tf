2a0bd2816e2f60de54a3308f7f1a184b65bc35c9
baserrato <benjamins@liatrio.com>
2023-01-26 10:42:00 -0800

    fix: updated terraform aks setup for sandbox

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,5 +1,5 @@
 variable "agent_count" {
-  default = 3
+  default = 1
 }
 
 # The following two variable declarations are placeholder references.
@@ -13,25 +13,9 @@ variable "aks_service_principal_client_secret" {
 }
 
 variable "cluster_name" {
-  default = "k8stest"
 }
 
 variable "dns_prefix" {
-  default = "k8stest"
-}
-
-# Refer to https://azure.microsoft.com/global-infrastructure/services/?products=monitor for available Log Analytics regions.
-variable "log_analytics_workspace_location" {
-  default = "eastus"
-}
-
-variable "log_analytics_workspace_name" {
-  default = "testLogAnalyticsWorkspaceName"
-}
-
-# Refer to https://azure.microsoft.com/pricing/details/monitor/ for Log Analytics pricing
-variable "log_analytics_workspace_sku" {
-  default = "PerGB2018"
 }
 
 variable "resource_group_location" {
@@ -39,11 +23,22 @@ variable "resource_group_location" {
   description = "Location of the resource group."
 }
 
-variable "resource_group_name_prefix" {
-  default     = "rg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+variable "resource_group_name" {
+  description = "Name of the resource group"
 }
 
 variable "ssh_public_key" {
   default = "~/.ssh/id_rsa.pub"
+}
+
+variable "tenant_id" {
+  sensitive = true
+}
+
+variable "client_secret" {
+  sensitive = true
+}
+
+variable "app_id" {
+  sensitive = true
 }
